1ac22c635564e06ed8e1bc38714e49618bbdaf5f
Harsh <h77776667777@gmail.com>
2025-07-30 17:22:49 +0530

    Update3 server.js

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,19 @@
+// 📁 backend/server.js
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from client folder
+app.use(express.static(path.join(__dirname, '../client')));
+
+// Serve index.html as root
 // Serve editor.html as home page
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/editor.html"));
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
 });
